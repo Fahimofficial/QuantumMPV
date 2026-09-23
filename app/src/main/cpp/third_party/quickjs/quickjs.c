77d104929db08c10de6c154cf7bcd681b279d568
Fahimofficial <55772687+Fahimofficial@users.noreply.github.com>
2026-09-23 17:39:15 +0000

    Add fast path for Array groupBy in QuickJS
    This commit implements a fast path for `Object.groupBy` when operating on dense arrays. It detects if the array utilizes the built-in array iterator and bypassing generic iterator function calls in favor of a direct fast-path element lookup using `js_get_fast_array_element` and fallback to `JS_GetPropertyInt64`.
    It also properly addresses a previously discovered spec-compliance bug in `groupBy` where it passed `ctx->global_obj` as the `this` argument to the mapping callback. The mapping callback is now correctly invoked with `JS_UNDEFINED` as the `this` value as dictated by the ECMAScript spec.
    Other bug fixes include correctly freeing `res` internally to prevent memory leaks, proper `JS_ToPropertyKey` conversion to conform to ECMAScript and properly disposing iterators if an exception occurs mid-iteration.

--- app/src/main/cpp/third_party/quickjs/quickjs.c
+++ app/src/main/cpp/third_party/quickjs/quickjs.c
@@ -41618,10 +41618,11 @@ static JSValue js_object_getOwnPropertySymbols(JSContext *ctx, JSValueConst this
 static JSValue js_object_groupBy(JSContext *ctx, JSValueConst this_val,
                                  int argc, JSValueConst *argv)
 {
-    JSValue res, iter, next, groups, k, v, prop;
+    JSValue res, iter, next, groups, k, v, prop, key;
     JSValueConst cb, args[2];
     int64_t idx;
     int done;
+    int is_array_iterator = 0;
 
     // "is function?" check must be observed before argv[0] is accessed
     cb = argv[1];
@@ -41639,6 +41640,7 @@ static JSValue js_object_groupBy(JSContext *ctx, JSValueConst this_val,
     v = JS_UNDEFINED;
     prop = JS_UNDEFINED;
     groups = JS_UNDEFINED;
+    key = JS_UNDEFINED;
 
     next = JS_GetProperty(ctx, iter, JS_ATOM_next);
     if (JS_IsException(next))
@@ -41648,46 +41650,117 @@ static JSValue js_object_groupBy(JSContext *ctx, JSValueConst this_val,
     if (JS_IsException(groups))
         goto exception;
 
+    // Fast path for arrays:
+    if (JS_VALUE_GET_TAG(iter) == JS_TAG_OBJECT) {
+        JSObject *iter_p = JS_VALUE_GET_OBJ(iter);
+        if (iter_p->class_id == JS_CLASS_ARRAY_ITERATOR) {
+            JSCFunctionType ft2 = { .iterator_next = js_array_iterator_next };
+            if (JS_IsCFunction(ctx, next, ft2.generic, 0)) {
+                is_array_iterator = 1;
+            }
+        }
+    }
+
+    if (is_array_iterator && JS_VALUE_GET_TAG(argv[0]) == JS_TAG_OBJECT) {
+        JSObject *p = JS_VALUE_GET_OBJ(argv[0]);
+        for (idx = 0; ; idx++) {
+            uint32_t len;
+            if (js_get_length32(ctx, &len, argv[0]))
+                goto exception_close;
+            if (idx >= len)
+                break;
+            if (!js_get_fast_array_element(ctx, p, idx, &v)) {
+                v = JS_GetPropertyInt64(ctx, argv[0], idx);
+                if (JS_IsException(v))
+                    goto exception_close;
+            }
+            args[0] = v;
+            args[1] = js_int64(idx);
+            k = JS_Call(ctx, cb, JS_UNDEFINED, 2, args);
+            if (JS_IsException(k))
+                goto exception_close;
+
+            key = JS_ToPropertyKey(ctx, k);
+            JS_FreeValue(ctx, k);
+            k = JS_UNDEFINED;
+            if (JS_IsException(key))
+                goto exception_close;
+
+            prop = JS_GetPropertyValue(ctx, groups, js_dup(key));
+            if (JS_IsException(prop))
+                goto exception_close;
+
+            if (JS_IsUndefined(prop)) {
+                prop = JS_NewArray(ctx);
+                if (JS_IsException(prop))
+                    goto exception_close;
+                if (JS_SetPropertyValue(ctx, groups, js_dup(key), js_dup(prop),
+                                        JS_PROP_C_W_E|JS_PROP_THROW) < 0) {
+                    goto exception_close;
+                }
+            }
+
+            res = js_array_push(ctx, prop, 1, vc(&v), /*unshift*/0);
+            if (JS_IsException(res))
+                goto exception_close;
+            JS_FreeValue(ctx, res);
+
+            JS_FreeValue(ctx, prop);
+            JS_FreeValue(ctx, key);
+            JS_FreeValue(ctx, v);
+            prop = JS_UNDEFINED;
+            key = JS_UNDEFINED;
+            v = JS_UNDEFINED;
+        }
+        JS_FreeValue(ctx, iter);
+        JS_FreeValue(ctx, next);
+        return groups;
+    }
+
     for (idx = 0; ; idx++) {
         v = JS_IteratorNext(ctx, iter, next, 0, NULL, &done);
         if (JS_IsException(v))
-            goto exception;
+            goto exception_close;
         if (done)
             break; // v is JS_UNDEFINED
 
         args[0] = v;
         args[1] = js_int64(idx);
-        k = JS_Call(ctx, cb, ctx->global_obj, 2, args);
+        k = JS_Call(ctx, cb, JS_UNDEFINED, 2, args);
         if (JS_IsException(k))
-            goto exception;
+            goto exception_close;
 
-        k = js_dup(k);
-        prop = JS_GetPropertyValue(ctx, groups, k);
+        key = JS_ToPropertyKey(ctx, k);
+        JS_FreeValue(ctx, k);
+        k = JS_UNDEFINED;
+        if (JS_IsException(key))
+            goto exception_close;
+
+        prop = JS_GetPropertyValue(ctx, groups, js_dup(key));
         if (JS_IsException(prop))
-            goto exception;
+            goto exception_close;
 
         if (JS_IsUndefined(prop)) {
             prop = JS_NewArray(ctx);
             if (JS_IsException(prop))
-                goto exception;
-            k = js_dup(k);
-            prop = js_dup(prop);
-            if (JS_SetPropertyValue(ctx, groups, k, prop,
+                goto exception_close;
+            if (JS_SetPropertyValue(ctx, groups, js_dup(key), js_dup(prop),
                                     JS_PROP_C_W_E|JS_PROP_THROW) < 0) {
-                goto exception;
+                goto exception_close;
             }
         }
 
         res = js_array_push(ctx, prop, 1, vc(&v), /*unshift*/0);
         if (JS_IsException(res))
-            goto exception;
+            goto exception_close;
+        JS_FreeValue(ctx, res);
         // res is an int64
 
         JS_FreeValue(ctx, prop);
-        JS_FreeValue(ctx, k);
+        JS_FreeValue(ctx, key);
         JS_FreeValue(ctx, v);
         prop = JS_UNDEFINED;
-        k = JS_UNDEFINED;
+        key = JS_UNDEFINED;
         v = JS_UNDEFINED;
     }
 
@@ -41695,9 +41768,13 @@ static JSValue js_object_groupBy(JSContext *ctx, JSValueConst this_val,
     JS_FreeValue(ctx, next);
     return groups;
 
+exception_close:
+    if (!JS_IsUndefined(iter))
+        JS_IteratorClose(ctx, iter, true);
 exception:
     JS_FreeValue(ctx, prop);
     JS_FreeValue(ctx, k);
+    JS_FreeValue(ctx, key);
     JS_FreeValue(ctx, v);
     JS_FreeValue(ctx, groups);
     JS_FreeValue(ctx, iter);
